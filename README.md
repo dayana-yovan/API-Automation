39fb05401375c873dfc9b6ec61099a723860bf78
dayana-yovan <dayanaanthony.a@gmail.com>
2021-10-20 17:50:07 +0530

    Framework Setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # API-Automation
+
+BDD Framework 
---
+++ src/test/java/runner/CucumberRunner.java
@@ -0,0 +1,9 @@
+package runner;
+
+public class CucumberRunner {
+
+	public CucumberRunner() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
